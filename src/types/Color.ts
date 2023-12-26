a911f8834032583bfe24090c39c33181db61666b
Stephen Choo <stephen.w.choo@gmail.com>
2023-12-26 11:41:57 +1100

    Set up Prettier, changed indentation, ASI and quotation use

--- src/types/Color.ts
+++ src/types/Color.ts
@@ -1,20 +1,24 @@
 import { Tuple } from "./Tuple";
 
 class Color extends Tuple {
-  constructor(r: number, g: number, b: number) {
-    super(r, g, b, 0);
-  }
+	constructor(r: number, g: number, b: number) {
+		super(r, g, b, 0);
+	}
 
-  times(scalar: number | Color) {
-    if (scalar instanceof Color) {
-      return new Color(this.x * scalar.x, this.y * scalar.y, this.z * scalar.z);
-    }
-    return new Color(this.x * scalar, this.y * scalar, this.z * scalar);
-  }
+	times(scalar: number | Color) {
+		if (scalar instanceof Color) {
+			return new Color(
+				this.x * scalar.x,
+				this.y * scalar.y,
+				this.z * scalar.z
+			);
+		}
+		return new Color(this.x * scalar, this.y * scalar, this.z * scalar);
+	}
 }
 
 function createColor(r: number, g: number, b: number) {
-  return new Color(r, g, b);
+	return new Color(r, g, b);
 }
 
-export { Color, createColor }
+export { Color, createColor };
